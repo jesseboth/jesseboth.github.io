756007c60ca5e78d83e5fb3dfa3140780f0c6e35
jesseboth <jessebot@buffalo.edu>
2021-03-15 16:39:52 -0400

    initial design

---
+++ code.js
@@ -0,0 +1,14 @@
+﻿nav_open = true;
+function open_nav(){
+    if(!nav_open){
+        document.getElementById("drawer").classList.remove("closed");
+        document.getElementById("drawer").classList.add("open");
+        document.getElementById("mySidenav").style.width = "0";
+    }
+    else{
+        document.getElementById("drawer").classList.remove("open");
+        document.getElementById("drawer").classList.add("closed");
+        document.getElementById("mySidenav").style.width = "200px";
+    }
+    nav_open = !nav_open
+}
