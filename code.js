38594aa4ddb90db1fe8220d62794864a52ddd471
jesseboth <jessebot@buffalo.edu>
2021-03-15 16:45:27 -0400

    update

--- code.js
+++ code.js
@@ -1,14 +1,21 @@
-﻿nav_open = true;
+﻿nav_open = false;
 function open_nav(){
     if(!nav_open){
         document.getElementById("drawer").classList.remove("closed");
         document.getElementById("drawer").classList.add("open");
-        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("mySidenav").style.width = "200px";
+        nav_open = !nav_open
     }
     else{
+        close_nav()
+    }
+}
+
+function close_nav(){
+    if(nav_open){
         document.getElementById("drawer").classList.remove("open");
         document.getElementById("drawer").classList.add("closed");
-        document.getElementById("mySidenav").style.width = "200px";
+        document.getElementById("mySidenav").style.width = "0";
+        nav_open = !nav_open
     }
-    nav_open = !nav_open
 }
