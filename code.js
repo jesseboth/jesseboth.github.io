47fce490b917135c436e507f13e3337824443157
jesseboth <jessebot@buffalo.edu>
2021-03-15 19:05:31 -0400

    tabs

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
-﻿nav_open = false;
+﻿window.onload = run
+
+nav_open = false;
 function open_nav(){
     if(!nav_open){
         document.getElementById("drawer").classList.remove("closed");
@@ -17,5 +19,24 @@ function close_nav(){
         document.getElementById("drawer").classList.add("closed");
         document.getElementById("mySidenav").style.width = "0";
         nav_open = !nav_open
+    }
+}
+
+function switch_content(content_display){
+    data = ["Home", "Projects", "AboutMe", "Contact"]
+    for(i = 0; i < data.length; i++){
+        document.getElementById(data[i]).classList.remove("Content");
+        document.getElementById(data[i]).classList.add("not_Content");
+    }
+
+    document.getElementById(content_display).classList.remove("not_Content");
+    document.getElementById(content_display).classList.add("Content");
+    location.hash = "-" + content_display
+}
+
+function run(){
+    var pound=window.location.href.split("#");
+    if(pound.length > 1){
+        switch_content(pound[1].substr(1))
     }
 }
